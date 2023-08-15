4c08640c07ec5d0fdffb5d29904181e1cfd394ac
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-15 15:36:14 +0200

    simple gpu task

---
+++ gpu_deep_learning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name="gpu_deep_learning.jl"
+#SBATCH --partition=gpu
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --gpus-per-task=2
+#SBATCH --mem-per-cpu=1G
+#SBATCH --account=research-<faculty>-<department>
+
+module load 2023r1 julia
+module load cuda/12.1
+
+previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
+
+srun julia gpu_deep_learning.jl > gpu_deep_learning.log
+
+/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
