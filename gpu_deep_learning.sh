b555caf7d2f70788b40ed12607ffef91bddc6d39
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-15 16:04:49 +0200

    basic gpu test

--- gpu_deep_learning.sh
+++ gpu_deep_learning.sh
@@ -2,12 +2,12 @@
 #
 #SBATCH --job-name="gpu_deep_learning.jl"
 #SBATCH --partition=gpu
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --gpus-per-task=2
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G
-#SBATCH --account=research-<faculty>-<department>
+#SBATCH --account=innovation
 
 module load 2023r1 julia
 module load cuda/12.1
