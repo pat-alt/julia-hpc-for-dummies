525b2fd5c257f601262b52ddce4be9fc1262653c
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-08-16 08:42:45 +0200

    now with gpu

--- gpu_deep_learning/gpu_deep_learning.sh
+++ gpu_deep_learning/gpu_deep_learning.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #
 #SBATCH --job-name="gpu_deep_learning.jl"
+#SBATCH --partition=gpu
 #SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
+# SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=innovation
 
 module load 2023r1 openmpi julia
-# module load cuda/12.1
+module load cuda/12.1
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
