bbeceb3e33b714fee314ef7831ede6e9832f62e1
pat-alt <altmeyerpat@gmail.com>
2023-08-21 15:04:41 +0200

    MPI now also working on local device

--- hello_mpi.jl
+++ hello_mpi.jl
@@ -1,11 +1,13 @@
-# examples/01-hello.jl
+using Pkg
+Pkg.activate(@__DIR__)
+
 using MPI
 MPI.Init()
 
 comm = MPI.COMM_WORLD
-println("Hello world, I am rank $(MPI.Comm_rank(comm)) of $(MPI.Comm_size(comm))")
+println("Hello world 🌍, I am rank $(MPI.Comm_rank(comm)) of world $(MPI.Comm_size(comm)).\n")
 MPI.Barrier(comm)
 
-println("P$(MPI.Comm_rank(comm)) behind barrier.")
+println("P$(MPI.Comm_rank(comm)) behind barrier 🚪.\n")
 
 MPI.Finalize()
