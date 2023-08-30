289b64ac783c553bc73a6bcb3ed8766c6d26b088
Patrick Altmeyer <paltmeyer@login1.hpc.tudelft.nl>
2023-08-30 14:17:52 +0200

    file runs, but not running jobs on multiple processes

--- hello_mpi_daic.sh
+++ hello_mpi_daic.sh
@@ -3,7 +3,7 @@
 #SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
 #SBATCH --time=0:01:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
-#SBATCH --cpus-per-task=2   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --cpus-per-task=4   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
 #SBATCH --mem=1024          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 #SBATCH --output=slurm_%j.out # Set name of output log. %j is the Slurm jobId
