0045e80edb3b6d92eec5c7b4f14a2e016a099380
pat-alt <altmeyerpat@gmail.com>
2023-08-30 14:03:30 +0200

    gpu test for daic

---
+++ gpu_dl_daic.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#SBATCH --partition=general # Request partition. Default is 'general' 
+#SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --time=0:01:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
+#SBATCH --cpus-per-task=2   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --mem=1024          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+#SBATCH --output=slurm_%j.out # Set name of output log. %j is the Slurm jobId
+#SBATCH --error=slurm_%j.err # Set name of error log. %j is the Slurm jobId
+
+#SBATCH --gres=gpu:1 # Request 1 GPU
+
+# Measure GPU usage of your job (initialization)
+previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2') 
+
+/usr/bin/nvidia-smi # Check sbatch settings are working (it should show the GPU that you requested)
+
+# Remaining job commands go below here. For example, to run python code that makes use of GPU resources:
+
+# Uncomment these lines and adapt them to load the software that your job requires
+module use /opt/insy/modulefiles          # Use DAIC INSY software collection
+module load cuda/11.2 cudnn/11.2-8.1.1.33 # Load certain versions of cuda and cudnn 
+srun julia gpu_dl.jl > gpu_dl.log
+
+# Measure GPU usage of your job (result)
+/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
+
+
+
