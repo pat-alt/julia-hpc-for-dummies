e7c43e619717ed3f59c26f816193d7f2c43bacae
pat-alt <altmeyerpat@gmail.com>
2023-09-07 16:26:16 +0200

    fixed ce_example.sh

--- ce_example_blue.sh
+++ ce_example_blue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name="Counterfactuals"
-#SBATCH --time=00:10:00
-#SBATCH --ntasks=8
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
@@ -10,4 +10,4 @@
 
 module load 2023rc1 openmpi julia
 
-srun julia hello_mpi.jl > hello_mpi.log
+srun julia ce_example.jl > ce_example.log
