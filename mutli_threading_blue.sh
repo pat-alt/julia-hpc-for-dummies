d949defe271746a8bdcc5c4a22d12789e8d78e23
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-08 13:48:52 +0200

    multi-threadin example

---
+++ mutli_threading_blue.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name="Multi-threading"
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=compute
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --account=innovation
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+
+module load 2023r1 openmpi julia
+
+srun julia --project multi_threading.jl > multi_threading.log
