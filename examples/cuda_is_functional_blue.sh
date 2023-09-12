c1ddc8e12585722f066019996e230faf846744b7
pat-alt <altmeyerpat@gmail.com>
2023-09-12 08:08:16 +0200

    gpu attemptes

--- examples/cuda_is_functional_blue.sh
+++ examples/cuda_is_functional_blue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name="Julia on GPU"
+#SBATCH --job-name="CUDA is functional"
 #SBATCH --partition=gpu
-#SBATCH --time=00:25:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=1
@@ -14,6 +14,6 @@ module load 2023r1
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
-srun julia --project cuda_is_functional.jl > cuda_is_functional.log
+srun julia --project=examples examples/cuda_is_functional.jl > examples/cuda_is_functional.log
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
--- examples/gpu_dl_blue.sh
+++ examples/gpu_dl_blue.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name="Julia on GPU"
+#SBATCH --job-name="Deep Learning on GPU"
 #SBATCH --partition=gpu
-#SBATCH --time=00:25:00
+#SBATCH --time=00:45:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
@@ -14,6 +14,6 @@ module load 2023r1
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
-srun julia --project gpu_dl.jl > gpu_dl.log
+srun julia --project=examples examples/gpu_dl.jl > examples/gpu_dl.log
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
