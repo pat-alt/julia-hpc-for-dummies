e1ca8cfadece6c56d55fec407ab251b3214e7823
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-12 17:45:28 +0200

    mljflux

--- examples/mljflux_gpu.jl
+++ examples/mljflux_gpu.jl
@@ -6,12 +6,15 @@ y, X = unpack(iris, ==(:Species), colname -> true, rng=123);
 X = Float32.(X)
 model = @load NeuralNetworkClassifier pkg=MLJFlux
 
+# Warm up:
 warmup = NeuralNetworkClassifier()
+warmup_gpu = NeuralNetworkClassifier(accelaration=CUDALibs())
+machine(warmup, X, y) |> fit!
+machine(warmup_gpu, X, y) |> fit!
+
+# Benchmark:
 clf = NeuralNetworkClassifier(epochs=1000)
 clf_gpu = NeuralNetworkClassifier(acceleration=CUDALibs(), epochs=1000)
-
-# Warm up:
-machine(warmup, X, y) |> x -> fit!(x; verbosity=1)
 
 @info "Training on CPU:"
 @time machine(clf, X, y) |> fit!
