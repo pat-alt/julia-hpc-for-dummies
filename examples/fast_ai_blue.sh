04fb3c676d4f8bd77ba190adf032f89be268d556
pat-alt <altmeyerpat@gmail.com>
2023-09-12 13:58:51 +0200

    gpu stuff

--- examples/fast_ai_blue.sh
+++ examples/fast_ai_blue.sh
@@ -14,6 +14,6 @@ module load 2023r1
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
-srun julia --project=examples examples/gpu_dl.jl > examples/gpu_dl.log
+srun julia --project=examples examples/fast_ai.jl > examples/fast_ai.log
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
