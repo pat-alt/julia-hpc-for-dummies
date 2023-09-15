be4ef93593b119656733d91389596f303f14ba52
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-15 17:09:18 +0200

    added JEM for testing

---
+++ examples/jem_gpu.jl
@@ -0,0 +1,20 @@
+using MLJ
+using MLJFlux
+using JointEnergyModels
+
+X, y = make_blobs(100, 3; centers=2, cluster_std=[1.0, 3.0])
+X = Float32.(X)
+model = @load NeuralNetworkClassifier pkg = MLJFlux
+
+𝒟x = Normal()
+𝒟y = Categorical(ones(2) ./ 2)
+sampler = ConditionalSampler(𝒟x, 𝒟y, input_size=size(Xplot)[1:end-1], batch_size=batch_size)
+clf = JointEnergyClassifier(
+    sampler;
+    builder=MLJFlux.MLP(hidden=(32, 32, 32,), σ=Flux.relu),
+    batch_size=batch_size,
+    finaliser=x -> x,
+    loss=Flux.Losses.logitcrossentropy,
+    acceleration=CUDALibs(),
+)
+mach = machine(clf, X, y) |> fit!
