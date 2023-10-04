ba943bf3904ceefad7d939ea091528d53e94940d
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-10-04 15:46:55 +0200

    let's see

--- examples/hello_mpi.jl
+++ examples/hello_mpi.jl
@@ -5,10 +5,20 @@ comm = MPI.COMM_WORLD
 
 if MPI.Comm_rank(comm) == 0
    println(VERSION)
+
+   # Multi-threading:
+   a = zeros(10)
+   Threads.@threads for i = 1:10
+      a[i] = Threads.threadid()
+   end
+
+   println(a)
+
 end
 
 MPI.Barrier(comm)
 
+# MPI:
 println("Hello world 🌍, I am rank $(MPI.Comm_rank(comm)) of world $(MPI.Comm_size(comm)).\n")
 MPI.Barrier(comm)
 
