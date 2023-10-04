a9b8d5ba5712ce182dd4d1d391510de4abc61c8d
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-10-04 15:25:18 +0200

    bleh

--- examples/hello_mpi_blue.sh
+++ examples/hello_mpi_blue.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name="hello_mpi.jl"
-#SBATCH --time=00:10:00
-#SBATCH --ntasks=8
+#SBATCH --time=00:05:00
+#SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=innovation
 
 module load 2023rc1 openmpi
+
+export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
 
 srun julia --project hello_mpi.jl > hello_mpi.log
--- examples/multi_threading_blue.sh
+++ examples/multi_threading_blue.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name="Multi-threading"
 #SBATCH --time=00:05:00
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
@@ -10,5 +10,7 @@
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
 module load 2023r1 openmpi julia
+
+export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
 
 srun julia --project --threads 8 multi_threading.jl > multi_threading.log
