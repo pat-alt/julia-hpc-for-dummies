4c44a7c4a283e2f8677bf38b547520d6dfa565a4
pat-alt <altmeyerpat@gmail.com>
2023-10-10 09:47:44 +0200

    trying with julia from software stack

--- examples/multi_threading_blue.sh
+++ examples/multi_threading_blue.sh
@@ -3,12 +3,13 @@
 #SBATCH --job-name="Multi-threading"
 #SBATCH --time=00:05:00
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=9
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=innovation
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 
+module load 2023r1 julia
 set -x
 
 export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
