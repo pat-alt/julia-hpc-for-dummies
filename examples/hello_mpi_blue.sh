0f3405804ed77afc4018f3bc0598ce0766b81663
pat-alt <altmeyerpat@gmail.com>
2023-10-10 08:48:31 +0200

    trying this again

--- examples/hello_mpi_blue.sh
+++ examples/hello_mpi_blue.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="hello_mpi.jl"
 #SBATCH --time=00:05:00
 #SBATCH --ntasks-per-node=5
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=innovation
@@ -13,4 +13,4 @@ set -x
 
 export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
 
-srun julia --project=examples --threads $SLURM_CPUS_PER_TASK examples/hello_mpi.jl > hello_mpi.log
+srun julia --project=examples --threads $SRUN_CPUS_PER_TASK examples/hello_mpi.jl > hello_mpi.log
