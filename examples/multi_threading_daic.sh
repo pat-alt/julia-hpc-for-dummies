e114920ba91fcb02e57646077c6c6289154a4fd5
pat-alt <altmeyerpat@gmail.com>
2023-10-16 15:41:24 +0200

    multi-threading example daic

---
+++ examples/multi_threading_daic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name="Multi-threading"
+#SBATCH --time=00:05:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --partition=general
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+
+source examples/slurm_header.sh
+
+srun julia --project=examples --threads $SRUN_CPUS_PER_TASK examples/multi_threading.jl > examples/multi_threading.log
