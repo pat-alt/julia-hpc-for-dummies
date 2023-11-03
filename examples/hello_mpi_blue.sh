fc035b1d7a04773092c70109cd9c5263effd7006
pat-alt <altmeyerpat@gmail.com>
2023-11-03 13:52:06 +0100

    bleh

--- examples/hello_mpi_blue.sh
+++ examples/hello_mpi_blue.sh
@@ -3,14 +3,13 @@
 #SBATCH --job-name="hello_mpi.jl"
 #SBATCH --time=00:05:00
 #SBATCH --ntasks-per-node=5
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --partition=compute
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=innovation
 
 module load 2023rc1 openmpi
-set -x
 
-export SRUN_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
+source examples/slurm_header.sh
 
 srun julia --project=examples --threads $SRUN_CPUS_PER_TASK examples/hello_mpi.jl > hello_mpi.log
