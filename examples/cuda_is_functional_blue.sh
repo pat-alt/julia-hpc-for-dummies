612767bb962eaca403d4e68b9a18ff0c48d4e86e
pat-alt <altmeyerpat@gmail.com>
2024-01-16 14:36:27 +0100

    just making sure CUDA functional actually works

--- examples/cuda_is_functional_blue.sh
+++ examples/cuda_is_functional_blue.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --job-name="CUDA is functional"
 #SBATCH --partition=gpu
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=innovation
@@ -16,6 +16,6 @@ source examples/slurm_header.sh
 
 previous=$(/usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/tail -n '+2')
 
-srun julia --project=examples examples/cuda_is_functional.jl > examples/cuda_is_functional.log
+srun julia --project=examples examples/cuda_is_functional.jl > examples/cuda_is_functional_blue.log
 
 /usr/bin/nvidia-smi --query-accounted-apps='gpu_utilization,mem_utilization,max_memory_usage,time' --format='csv' | /usr/bin/grep -v -F "$previous"
