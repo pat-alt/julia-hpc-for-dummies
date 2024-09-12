d0ae4914f0ab49aab2fd87dd99e9aa70e9361b9a
pat-alt <altmeyerpat@gmail.com>
2024-09-12 17:37:51 +0200

    trying again

--- examples/cuda_is_functional_blue.sh
+++ examples/cuda_is_functional_blue.sh
@@ -2,15 +2,13 @@
 #
 #SBATCH --job-name="CUDA is functional"
 #SBATCH --partition=gpu
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-task=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=3G
 #SBATCH --account=innovation
 #SBATCH --mail-type=END
-
-module load 2024r1
 
 source examples/slurm_header.sh
 
